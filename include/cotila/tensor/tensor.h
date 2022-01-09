1c96623b2200e28154ac24ca7e8467c8a3bfa658
David Rubinstein <drubinstein91@gmail.com>
2022-01-09 14:10:22 -0500

    simplified tensor impl

--- include/cotila/tensor/tensor.h
+++ include/cotila/tensor/tensor.h
@@ -5,64 +5,51 @@
 #ifndef COTILA_TENSOR_TENSOR_H_
 #define COTILA_TENSOR_TENSOR_H_
 
-#include <cotila/detail/tmp.h>
 #include <cotila/detail/assert.h>
+#include <cotila/detail/tmp.h>
+#include <cotila/vector/vector.h>
+
 #include <cstdint>
 #include <type_traits>
 
 namespace cotila {
 
-template <typename T, std::size_t... Dimension>
-struct tensor_impl;
+template <typename T, std::size_t Dimension, std::size_t... Shape>
+struct tensor {
+  // TODO: Add an assertion for non as 0
+  COTILA_DETAIL_ASSERT_ARITHMETIC(T)
 
-template <typename T, std::size_t... Dimension>
-using tensor_impl_t = typename tensor_impl<T, Dimension...>::type;
+  using value_type = T;
+  using size_type = std::size_t;
 
-template <typename T>
-struct tensor_impl<T> {
-    using type = T;
+  constexpr tensor<T, Shape...> operator[](std::size_t i) noexcept {
+    return array[i];
+  }
+  constexpr const tensor<T, Shape...> operator[](size_type i) const noexcept {
+    return array[i];
+  }
+
+  constexpr const tensor<T, std::size_t d, std::size_t s...> &at(
+      std::size_t d, std::size_t s...) {
+    return array[d].at(s...);
+  }
+
+  tensor<T, Shape...> array[Dimension];  ///< @private
 };
 
-template <typename T, std::size_t First, std::size_t... Dimension>
-struct tensor_impl<T, First, Dimension...> {
-    using type = tensor_impl_t<T, Dimension...>[First];
+template <typename T, std::size_t Dimension>
+struct tensor<T, Dimension> {
+  // TODO: Add an assertion for non as 0
+  COTILA_DETAIL_ASSERT_ARITHMETIC(T)
+
+  using value_type = T;
+  using size_type = std::size_t;
+  constexpr T operator[](std::size_t i) noexcept { return array[i]; }
+  constexpr const T operator[](size_type i) const noexcept { return array[i]; }
+
+  T array[Dimension];  ///< @private
 };
 
-/** @brief A container representing a tensor
- *  @tparam T scalar type to contain
- *  @tparam Shape The shape of the tensor
- * 
- *  `cotila::tensor` is a container representing a tensor.
- * 
- *  It is an aggregate type more similar, but more abstract than `cotila::matrix`
- *  and can be initialized with aggregate initialization 
- *  or with the `cotila::make_tensor` function.
- */
- template <typename T, std::size_t... Shape> struct tensor {
-   // TODO: Add an assertion for non as 0
-   COTILA_DETAIL_ASSERT_ARITHMETIC(T)
+}  // namespace cotila
 
-   using value_type = T;
-   using size_type = std::size_t;
-   static constexpr size_type shape[sizeof...(Shape)] = {Shape...};
-
-   
-  /** @name Access specific element from leading dimension */
-  ///@{
-  /** @brief Access specified dimension
-   *  @param i index of the slice to extract. Must be same rank as the shape.
-   *  @return The selected element from the leading dimension
-   *
-   *  Given a Tensor with shape (x,y,z,...) return T[i][y][z]...
-   */
-    constexpr auto operator[](std::size_t i) -> auto { return arrays[i]; }
-
-   tensor_impl<T, Shape...> arrays;
- };
-
-template <typename T, std::size_t... Shape>
-tensor(const tensor_impl<T, Shape...>)->tensor<T, Shape...>;
-
-} // namespace cotila
-
-#endif // COTILA_TENSOR_TENSOR_H_
+#endif  // COTILA_TENSOR_TENSOR_H_
