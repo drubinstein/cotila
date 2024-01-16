89a27ee8aa996b820754e4d6826be61aa01d98e2
David Rubinstein <drubinstein91@gmail.com>
2024-01-15 21:27:29 -0500

    Working tensor slice + simple test

--- include/cotila/tensor/tensor.h
+++ include/cotila/tensor/tensor.h
@@ -10,9 +10,14 @@
 #include <cotila/vector/vector.h>
 
 #include <cstdint>
+#include <tuple>
 #include <type_traits>
+#include <utility>
 
 namespace cotila {
+
+template <std::size_t...>
+struct shape {};
 
 template <typename T, std::size_t Dimension, std::size_t... Shape>
 struct tensor {
@@ -32,14 +37,18 @@ struct tensor {
   constexpr tensor<T, Shape...> *begin() noexcept { return array; }
   constexpr tensor<T, Shape...> *end() noexcept { return array + Dimension; }
   constexpr const tensor<T, Shape...> *cbegin() const noexcept { return array; }
-  constexpr const tensor<T, Shape...> *cend() const noexcept { return array + Dimension; }
+  constexpr const tensor<T, Shape...> *cend() const noexcept {
+    return array + Dimension;
+  }
 
-  template <std::size_t Begin, std::size_t... Begins, std::size_t Size, std::size_t... Sizes>
-  constexpr tensor<T, Size, Sizes...> slice() const noexcept {
+  template <std::size_t Begin, std::size_t... Begins, std::size_t Size,
+            std::size_t... Sizes>
+  constexpr auto slice(const shape<Begin, Begins...>,
+                       const shape<Size, Sizes...>) const noexcept {
     tensor<T, Size, Sizes...> sliced;
-    for (std::size_t i = Begin; i < Begin + Size; ++i) {
-      sliced[i] = array[i].template slice<Begins..., Sizes...>();
-    }
+    std::transform(
+        array + Begin, array + Begin + Size, sliced.array,
+        [](auto x) { return x.slice(shape<Begins...>(), shape<Sizes...>()); });
     return sliced;
   }
 
@@ -55,48 +64,46 @@ struct tensor<T, Dimension> {
   using size_type = std::size_t;
   constexpr T operator[](std::size_t i) noexcept { return array[i]; }
   constexpr const T operator[](size_type i) const noexcept { return array[i]; }
-  
+
   constexpr T *begin() noexcept { return array; }
   constexpr T *end() noexcept { return array + Dimension; }
   constexpr const T *cbegin() const noexcept { return array; }
   constexpr const T *cend() const noexcept { return array + Dimension; }
-  
+
   template <std::size_t Begin, std::size_t Size>
-  constexpr tensor<T, Size> slice() const noexcept {
+  constexpr auto slice(const shape<Begin>, const shape<Size>) const noexcept {
     tensor<T, Size> sliced;
-    for (std::size_t i = Begin; i < Begin + Size; ++i) {
-      sliced[i] = array[i];
-    }
+    std::copy_n(array + Begin, Size, sliced.array);
     return sliced;
   }
 
   T array[Dimension];  ///< @private
 };
 
-/** \addtogroup tensor 
+/** \addtogroup tensor
  *  @{
  */
 
 /** @name cotila::tensor deduction guides */
 ///@{
 
+// TODO: Can this be done with a variadic macro?
 /** @brief deduction guide for aggregate initialization
  *  @relatesalso cotila::tensor
  *
  *  This deduction guide allows cotila::tensor to be constructed like this:
  *  \code{.cpp}
- *  cotila::tensor m{{{1., 2.}, {3., 4.}}}; // deduces the type of m to be cotila::tensor<double, 2, 2>
- *  \endcode
+ *  cotila::tensor m{{{1., 2.}, {3., 4.}}}; // deduces the type of m to be
+ * cotila::tensor<double, 2, 2> \endcode
  */
 template <typename T, std::size_t M>
-tensor(const T (&)[M])->tensor<T, M>;
+tensor(const T (&)[M]) -> tensor<T, M>;
 
 template <typename T, std::size_t M, std::size_t N>
-tensor(const T (&)[M][N])->tensor<T, M, N>;
+tensor(const T (&)[M][N]) -> tensor<T, M, N>;
 
 template <typename T, std::size_t M, std::size_t N, std::size_t O>
-tensor(const T (&)[M][N][O])->tensor<T, M, N, O>;
-
+tensor(const T (&)[M][N][O]) -> tensor<T, M, N, O>;
 
 ///@}
 
--- test/tensor_test.h
+++ test/tensor_test.h
@@ -26,11 +26,14 @@ constexpr tensor<double, 1, 3, 3> t133 = {{
 }};
 
 constexpr tensor<double, 3, 3, 1> t331 = {{
-    {1., 1., 1.}, 
-    {1., 1., 1.}, 
-    {1., 1., 1.}
+    {1., 2., 3.}, 
+    {4., 5., 6.}, 
+    {7., 8., 9.}
 }};
 
+constexpr auto t331s = t331.slice(shape<1,1,0>(), shape<1, 1, 1>());
+
+static_assert(t331s[0][0][0] == 5., "tensor slice");
 }  // namespace test
 }  // namespace cotila
 
