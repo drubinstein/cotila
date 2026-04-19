8ebfb14898a0a323f3618909e77fdefa85c9e256
David Rubinstein <drubinstein91@gmail.com>
2026-04-19 15:30:11 -0400

    tensor: re-enable slice and all baseline tests

--- test/tensor_test.h
+++ test/tensor_test.h
@@ -10,22 +10,12 @@ namespace test {
 
 constexpr tensor<double, 1> t1 = {{1.0}};
 constexpr tensor<double, 3> t3 = {{1.0, 1.0, 1.0}};
-
-static_assert((t1 + 1.0)[0] == 2.0, "tensor + scalar");
-static_assert((t3 * 3.0)[1] == 3.0, "tensor * scalar");
-
 constexpr tensor<double, 3, 1> t31 = {{1.0, 3.0, 1.0}};
 constexpr tensor<double, 3, 3> t33 = {{
     {1.0, 1.0, 1.0},
     {1.0, 1.0, 1.0},
     {1.0, 1.0, 1.0}
 }};
-
-static_assert((t31 / 3.0)[1][0] == 1.0, "tensor / scalar");
-static_assert((t33 - 3.0)[1][0] == -2.0, "tensor - scalar");
-
-// Remaining tests re-enabled progressively.
-#if 0
 constexpr tensor<double, 1, 3, 3> t133 = {{
     {{
         {1.0, 1.0, 1.0},
@@ -43,7 +33,11 @@ constexpr tensor<double, 3, 3, 1> t331 = {{
 constexpr auto t331s = t331.slice(shape<1, 1, 0>(), shape<1, 1, 1>());
 
 static_assert(t331s[0][0][0] == 5.0, "tensor slice");
-#endif
+
+static_assert((t1 + 1.0)[0] == 2.0, "tensor + scalar");
+static_assert((t3 * 3.0)[1] == 3.0, "tensor * scalar");
+static_assert((t31 / 3.0)[1][0] == 1.0, "tensor / scalar");
+static_assert((t33 - 3.0)[1][0] == -2.0, "tensor - scalar");
 
 }  // namespace test
 }  // namespace cotila
